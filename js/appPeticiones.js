b67cf2831c519cf81e54b1f521c315ba6ec7974e
Facundo <77032020+facundoangel@users.noreply.github.com>
2022-10-09 20:07:01 -0300

    cambio de host

--- js/appPeticiones.js
+++ js/appPeticiones.js
@@ -1,6 +1,6 @@
 const cargaScript = () => {
   //DIRECCIÓN SERVIDOR DONDE SE MONTA EL BACKEND
-  const RutaServidor = "http://deployprueba.somee.com/";
+  const RutaServidor = "https://pruebadeployapi.azurewebsites.net/";
 
   //===============FUNCIONES DE PETICIONES AJAX========================
   //BORRAR EQUIPO
